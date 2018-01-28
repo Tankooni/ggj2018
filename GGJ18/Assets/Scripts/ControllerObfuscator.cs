229eb8773a52147bccc51240357e4b3c9a33b372
cloudaran <cloudaran@gmail.com>
2018-01-28 00:12:40 -0500

    Added a script that can be attached to controllers to hide the default model.

---
+++ GGJ18/Assets/Scripts/ControllerObfuscator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class ControllerObfuscator : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+        GameObject controller =
+            this.gameObject;
+
+        VRTK_ObjectAppearance.SetRendererHidden(controller);
+    }
+}
